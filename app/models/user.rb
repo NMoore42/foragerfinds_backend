ac74f46c2272713cb1074b996954639720d958f0
NMoore42 <nicholas.moore5@icloud.com>
2020-10-06 14:44:17 -0500

    add model relationships and basic test seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :relationships_where_follower, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  has_many :foragers, through: :relationships_where_follower, source: :forager
+  has_many :relationships_where_forager, class_name: "Relationship", foreign_key: :forager_id, dependent: :destroy
+  has_many :followers, through: :relationships_where_forager, source: :follower
+  has_many :posts
 end
