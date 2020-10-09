efbc32e9a6c2de7f8400a31b42aaeb1627e76e42
NMoore42 <nicholas.moore5@icloud.com>
2020-10-09 10:47:59 -0500

    add standard routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show, :destroy, :update]
+  resources :relationships, only: [:create, :destroy, :update]
+  resources :posts, only: [:create, :show, :destroy, :update]
+  resources :comments, only: [:create, :show, :destroy, :update]
 end
