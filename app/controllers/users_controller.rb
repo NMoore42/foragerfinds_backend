e8d89b9a3ca4107b2e7a6e2f6d9067c535bb7fe0
NMoore42 <nicholas.moore5@icloud.com>
2020-10-13 10:16:10 -0500

    rack cors enabled

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+  def create
+    byebug
+    # Need to review JWT auth rails https://learn.co/tracks/module-4-web-development-immersive-2-1/auth/jwt-auth-in-rails/jwt-auth-rails
+  end
+end
