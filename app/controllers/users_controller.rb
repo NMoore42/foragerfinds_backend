05c9797e752c11fc0ddc7973fc7cd26732dc9844
NMoore42 <nicholas.moore5@icloud.com>
2020-10-19 15:13:28 -0500

    removed tmp log and byebug tracked files, changed from instance variables to variables in user and auth controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,19 @@ class UsersController < ApplicationController
     render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
 
+
+  #Register
   def create
-    @user = User.create(user_params)
-    if @user.valid?
-      @token = encode_token(user_id: @user.id)
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    user = User.create(user_params)
+    if user.valid?
+      token = encode_token(user_id: user.id)
+      render json: { user: UserSerializer.new(user), jwt: token }, status: :created
     else
       render json: { error: "Failed to create user" }, status: :not_acceptable
     end
   end
+
+
 
   private
 
